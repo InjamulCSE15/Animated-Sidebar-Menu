9a42df3054ea012c8d44775fd2df7a28614d766c
unknown <yeasin.mohammod018@gmail.com>
2022-03-26 11:22:15 +0600

    restart my project

--- JS/myApp.js
+++ JS/myApp.js
@@ -1,6 +1,7 @@
 // console.log("JavaScript file added.");
 
 const shrinkBtn = document.querySelector(".shrink-btn");
+const search = document.querySelector(".search");
 
 shrinkBtn.addEventListener("click", () => {
    // console.log("Button Clicked");
@@ -8,8 +9,12 @@ shrinkBtn.addEventListener("click", () => {
    shrinkBtn.classList.add("hovered");
 
    setTimeout(
-       () => {
-        shrinkBtn.classList.remove("hovered");
-       }, 500);
+    () => {
+    shrinkBtn.classList.remove("hovered");
+    }, 500);
+});
 
-})
+search.addEventListener("click", () => {
+    document.body.classList.remove("shrink");
+    search.lastElementChild.focus();
+});
